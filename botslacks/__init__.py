6a78fd7fd92f7d9a8f0e0fb7a57dc6bd5e12ab57
saml <skynare@gmail.com>
2015-10-08 10:49:25 -0400

    wow

---
+++ botslacks/__init__.py
@@ -0,0 +1,154 @@
+import asyncio
+import logging
+import json
+from collections import namedtuple
+
+import aiohttp
+import websockets
+
+log = logging.getLogger(__name__)
+
+SubCommand = namedtuple('SubCommand', ['argspec', 'help', 'func'])
+
+def _find_widths(commands):
+    command_width = 0
+    argspec_width = 0
+    for command,subcommand in commands.items():
+        command_length = len(command)
+        argspec_length = len(subcommand.argspec)
+        if command_length > command_width:
+            command_width = command_length
+        if argspec_length > argspec_width:
+            argspec_width = argspec_length
+
+    return command_width,argspec_width
+
+def help_message(commands):
+    '''Given dict of command (str) to SubCommand, returns help string.'''
+    command_width,argspec_width = _find_widths(commands)
+    msg = []
+    for command,subcommand in commands.items():
+        msg.append('{} {} -- {}'.format(command.rjust(command_width), subcommand.argspec.rjust(argspec_width), subcommand.help))
+    return '''```{}```'''.format('\n'.join(msg))
+
+def configure_logging(log):
+    '''default logging configuration'''
+    if len(log.handlers) == 0:
+        log_level = logging.DEBUG
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler = logging.StreamHandler()
+        handler.setLevel(log_level)
+        handler.setFormatter(formatter)
+        log.setLevel(log_level)
+        log.addHandler(handler)
+
+def parse_args(s):
+    '''
+    'foo bar a b c' => ('foo', 'bar a b c')
+    'foo' => ('foo', '')
+    '''
+    splitted = s.split(maxsplit=1)
+    if len(splitted) >= 2:
+        return splitted[0],splitted[1]
+    if len(splitted) == 1:
+        return splitted[0],''
+    return '',''
+
+class SlackError(Exception):
+    pass
+
+class SlackBot(object):
+    def __init__(self, token, url='https://slack.com/api/'):
+        self.token = token
+        self.params = {'token': token}
+        self.url = url
+        self.ws_url = None
+        self.channel_names = {} # channel or group id to names
+        self.user_names = {}
+        self.bot_id = None
+        self.bot_name = None
+        self.command_handlers = {}
+
+    def register_command(self, command, func):
+        if command in self.command_handlers:
+            raise SlackError('Already registered: {}'.format(command))
+        self.command_handlers[command] = func
+
+    def _upsert_user(self, user):
+        self.user_names[user['id']] = user['name']
+
+    def _upsert_channel(self, channel):
+        self.channel_names[channel['id']] = channel['name']
+
+    def _init_login_data(self, d):
+        about_me = d['self']
+        self.bot_id = about_me['id']
+        self.bot_name = about_me['name']
+
+        for u in d.get('bots', []):
+            self._upsert_user(u)
+
+        for u in d.get('users', []):
+            self._upsert_user(u)
+
+        for c in d.get('channels', []):
+            self._upsert_channel(c)
+
+        for c in d.get('groups', []):
+            self._upsert_channel(c)
+            
+    def _calculate_prefix(self, msg):
+        if msg['channel'].startswith('D'): #don't want to address in direct message
+            return ''
+
+        prefix = self.user_names.get(msg['user'], '')
+        if prefix:
+            prefix = prefix + ', '
+        return prefix
+
+    @asyncio.coroutine
+    def _fetch(self, cmd):
+        url = self.url + cmd
+        resp = yield from aiohttp.request('GET', url, params=self.params)
+        return (yield from resp.json())
+
+    @asyncio.coroutine
+    def start(self):
+        d = yield from self._fetch('rtm.start')
+        if not d['ok']:
+            raise SlackError(d.get('error'))
+        self._init_login_data(d)
+        log.debug(d)
+        log.info('Channels: %s', ', '.join(self.channel_names.values()))
+        log.info('Users: %s', ', '.join(self.user_names.values()))
+
+        self.ws_url = d['url']
+        ws = yield from websockets.connect(self.ws_url)
+        response_id = 1
+        while True:
+            msg = yield from ws.recv()
+            msg = json.loads(msg)
+            log.debug('Recieved %s', msg)
+            msg_type = msg.get('type')
+            if msg_type == 'message':
+                user_id = msg['user']
+                if user_id != self.bot_id:
+                    response = self._calculate_response(msg, response_id)
+                    if response:
+                        log.info('Sending %s', response)
+                        yield from ws.send(json.dumps(response))
+                        response_id += 1
+            elif msg_type == 'channel_joined' or msg_type == 'group_joined':
+                self._upsert_channel(msg['channel'])
+                log.info('Channels: %s', ', '.join(self.channel_names.values()))
+
+    def _calculate_response(self, msg, response_id):
+        input_text = msg['text']
+        command,args = parse_args(input_text)
+        f = self.command_handlers.get(command)
+        if f is not None:
+            response_text = f(args)
+            if response_text:
+                prefix = self._calculate_prefix(msg)
+                return {'id': response_id, 'type': 'message', 'channel': msg['channel'], 'text': prefix + response_text}
+
---
+++ botslacks/commands/jenkins.py
@@ -0,0 +1,89 @@
+import asyncio
+import re
+from collections import namedtuple
+import operator
+
+import aiohttp
+
+from botslacks import log, parse_args, SubCommand, help_message
+
+NON_WORD = re.compile(r'[\d\W]+')
+
+JenkinsJob = namedtuple('JenkinsJob', ['name', 'url'])
+
+
+def parse_jobs(jobs):
+    '''[{name:..., url:...}, ...] => {key: JenkinsJob}'''
+    d = {}
+    for job in jobs:
+        name = job['name']
+        job_url = job['url']
+        key = NON_WORD.sub('', name.lower())
+        d[key] = JenkinsJob(name, job_url)
+    return d
+    
+
+class Jenkins(object):
+
+    def __init__(self, url, auth):
+        '''
+        url is jenkins base url.
+        auth is (username,token).
+        '''
+        self.url = url
+        username,password = auth
+        self.auth = aiohttp.BasicAuth(username, password)
+        self.http = aiohttp.ClientSession(auth=self.auth)
+        self.jobs = {}
+        self.commands = {
+            'info': SubCommand('<project name>', 'displays project information.', self.info),
+            'help': SubCommand('', 'displays this message.', self.help),
+        }
+    
+
+    @asyncio.coroutine
+    def init(self):
+        yield from self.reload_jobs()
+
+    @asyncio.coroutine
+    def reload_jobs(self):
+        self.jobs = yield from self.fetch_all_jobs()
+        return self.jobs
+
+    def find_job(self, s):
+        words = s.lower().split()
+        scores = {k:0 for k in self.jobs.keys()}
+        for word in words:
+            for k,score in scores.items():
+                if word in k:
+                    scores[k] += 1
+        l = sorted(scores.items(), key = operator.itemgetter(1), reverse=True)
+        if l:
+            top_match = l[0]
+            key,score = top_match
+            log.info('Found %s from input "%s"', key, s)
+            return self.jobs[key]
+
+
+    @asyncio.coroutine
+    def fetch_all_jobs(self):
+        url = self.url + '/api/json'
+        r = yield from self.http.get(url)
+        d = yield from r.json()
+        return parse_jobs(d['jobs'])
+
+    def info(self, text):
+        if text:
+            job = self.find_job(text)
+            if job:
+                return 'Found {} ({})'.format(job.name, job.url)
+
+    def help(self, text):
+        return 'Available subcommands ' + help_message(self.commands)
+        
+    def process(self, text):
+        command,args = parse_args(text)
+        subcommand = self.commands.get(command)
+        if subcommand:
+            return subcommand.func(args)
+
